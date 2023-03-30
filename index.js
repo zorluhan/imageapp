993400fcf47a08c8827a91816385bb0d4ba8fa7f
zorluhan <zorluhan@gmail.com>
2023-03-30 22:07:44 +0300

    lklklllklkl

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
  
-const express = require('express');
-const app = express();
+ 
 const apiKey = process.env.API_KEY;
 
 app.get('/api-key', (req, res) => {
   res.send(apiKey);
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+ 
  
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
