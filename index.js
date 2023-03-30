cdba6a03b21c2e7786f61ae327fab9ce83da56b2
zorluhan <zorluhan@gmail.com>
2023-03-30 21:34:12 +0300

    lkllj

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const apiKey = process.env.API_KEY;
+
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,3 +17,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+
