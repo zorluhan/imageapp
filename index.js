6857a3acbc0ba426047e4c1163f53aca4256dd20
zorluhan <zorluhan@gmail.com>
2023-03-30 22:02:00 +0300

    lklklklklkf

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
  
 // Serve static files from the public directory
