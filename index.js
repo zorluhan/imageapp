6a2a7857762b6cc86b97eb9862dfb94f7656c96f
zorluhan <zorluhan@gmail.com>
2023-03-30 20:21:12 +0300

    kkk

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+/* 
+    Incase you are using mongodb atlas database uncomment below line
+    and replace "mongoAtlasUri" with your mongodb atlas uri.
+*/
+// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+let http = require('http');
+let fs = require('fs');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
