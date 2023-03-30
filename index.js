ea4305379db5ed9d6e72bd6ec23cf3b0fe8accc6
zorluhan <zorluhan@gmail.com>
2023-03-30 20:32:19 +0300

    efefj

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get('/', (req, res) => {
-  res.send('index.html', {root: './'})
+  res.send('index.html')
 })
  
 app.listen(port, () => {
